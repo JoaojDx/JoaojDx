d13543b92c4cf120ffbe6ffe09ea822e89cc92c7
unknown <funnyazevedo@gmail.com>
2024-10-04 04:17:51 -0300

    1, 3 e 4 resolvidos

--- Exercicio-3.c
+++ Exercicio-3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    int INDICE = 12, SOMA = 0, K = 1;
 
+    while(K < INDICE){
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+    printf("\n SOMA = %i", SOMA); //Imprime 77
 
 }//main
--- Exercicio-4.c
+++ Exercicio-4.c
@@ -2,5 +2,11 @@
 
 int main(){
 
+printf("\n a) - 9"); // num + 2
+printf("\n b) - 128"); // num * 2
+printf("\n c) - 49"); // num ^ 2 (ao quadrado)
+printf("\n d) - 100"); // numPAR * numPAR (2*2 4*4 6*6 8*8)
+printf("\n e) - 13"); // Sequencia de Fibonacci
+printf("\n f) - 200"); // Numeros que comecam com a letra D
 
 }//main
