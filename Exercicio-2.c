268d14802de205cc97399d6510e128138129cdf9
JoaojDx <funnyazevedo@gmail.com>
2024-10-17 03:12:37 -0300

    finalfinal

--- Exercicio-2.c
+++ Exercicio-2.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(){
+char res[30];
+    int i, tam, cont = 0;
+    char *pr = NULL;
+    pr = res;
 
+    printf("Insira uma frase: ");
+    gets(pr);
+    fflush(stdin);
+
+    tam = strlen(pr);
+
+    for(i=0;i<tam;i++){
+            if(*(pr+i) == 'a' || *(pr+i) == 'A'){
+            cont++;
+            }
+    }
+
+    if(cont > 0)
+        printf("\nCaractere 'a'/'A' detectado");
+    else
+        printf("\nCaractere 'a'/'A' nao foi detectado");
+
+    printf("\n\nA frase e: \n\n%s\n\nA letra 'a'/'A' aperece %i vez(es)\n", res, cont);
+
+        return 0;
 
 }//main
--- Exercicio-5.c
+++ Exercicio-5.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-
+printf("\nPrimeiro, ligar o primeiro interruptor e deixar ligado por cerca de 10 minutos.");
+printf("\nApós esse tempo, desligar e acionar o segundo interruptor.");
+printf("\nEm seguida, ir às lâmpadas. A lâmpada que estiver acesa corresponde ao segundo interruptor.");
+printf("\nSe uma lâmpada está desligada, mas ainda quente, essa é do primeiro interruptor.");
+printf("\nPor fim, a lâmpada que estiver fria e desligada pertence ao terceiro interruptor.");
 
 }//main
